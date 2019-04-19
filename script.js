cb85f75fd36ce0fb70ffbb861bc061df7b7c2b31
Andres <andreshuz0709@gmail.com>
2019-04-19 01:01:55 +0000

    api is working and character select is working

--- script.js
+++ script.js
@@ -1,17 +1,52 @@
 /*global $*/
+console.log("hi");
 var randomCharacters = Math.floor(Math.random() * 20);
+
+
 function characterChose(){
-    /*var url = "https://rickandmortyapi.com/api/character/"*/
+    var characterSearch = $("#searchInput").val();
+    var url = "https://rickandmortyapi.com/api/character/?name=" + characterSearch ;
     $.ajax({
-        url:"https://rickandmortyapi.com/api/character/",
+        url: url,
         method: "GET",
         success: function(response){
-        response.results.forEach(function(x){
-         $("#main").append("<img src=" + x.image + ">" + "</img>");   
-        });
+            if(response.results[0].species === "Human"){
+                $("#categoryOne").append("<img src=" + response.results[0].image + ">", response.results[0].name);
+            }
+            else if(response.results[0].species === "unknown"){
+                $("#categoryThree").append("<img src=" + response.results[0].image + ">", response.results[0].name);
+            }
+            else if(response.results[0].species === "Alien"){
+                $("#categoryTwo").append("<img src=" + response.results[0].image + ">", response.results[0].name);
+            }
+            
         }
     });
     
     
 }
-characterChose();
+
+$("#searchButton").click(function(){
+    characterChose();
+    
+});
+
+function famousLocations(){
+    var locationUrl = "https://rickandmortyapi.com/api/location/";
+    $.ajax({
+        url: locationUrl,
+        method: "GET",
+        success: function(response){
+        $("body").append(response.results.name);  
+            console.log(response.results.name);
+        }
+    
+});
+
+    
+}
+
+$("#locations").click(function(){
+    famousLocations();
+    
+});
